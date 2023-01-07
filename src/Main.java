e9a2b59708188f07cf110296184b31fde7552610
Bolot Ashirkulov <ashirkulovbolott@gmail.com>
2023-01-07 14:37:32 +0600

    changed something in class Simulation.java and Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-import java.util.List;
+import java.nio.file.Path;
 
 public class Main {
+    static FileService service = new FileService(Path.of("data/cats.json"));
     public static void main(String[] args) {
+
         System.out.println("Wish Success");
+        var cats = service.getCats();
         Simulation simulation = new Simulation();
-        simulation.displayInfoOfCats(simulation.sortByAverageLvl(List.of()));
+        simulation.displayInfoOfCats(simulation.sortByAverageLvl(cats));
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -4,15 +4,15 @@ import java.util.stream.Collectors;
 
 public class Simulation {
     public void displayInfoOfCats(List<Cat> cats) {
-        System.out.println("*---*-----------*---------*----------*------------*---------*---------------*");
-        System.out.println(" #  | NAME      | AGE     | HEALTH   | MOOD       | SATIETY | AVERAGE LEVEL |");
-        System.out.println("*---*-----------*---------*----------*------------*---------*---------------*");
+        System.out.println("*----*-----------*---------*----------*------------*---------*---------------*");
+        System.out.println("| #  | NAME      | AGE     | HEALTH   | MOOD       | SATIETY | AVERAGE LEVEL |");
+        System.out.println("*----*-----------*---------*----------*------------*---------*---------------*");
         for (int i = 0; i < cats.size(); i++) {
-            System.out.printf("| %-1s | %-9s |  %-4s |  %-5s |  %-6s |  %-4s |  %-7s |%n", i + 1, cats.get(i).getName(),
+            System.out.printf("| %-2s | %-9s |    %-4s |    %-5s |     %-6s |    %-4s |       %-7s |%n", i + 1, cats.get(i).getName(),
                     cats.get(i).getAge(), cats.get(i).getHealthLevel(), cats.get(i).getSpiritLevel(),
                     cats.get(i).getSatietyLevel(), cats.get(i).getAverage());
         }
-        System.out.println("*---*-----------*---------*----------*------------*---------*---------------*\n");
+        System.out.println("*----*-----------*---------*----------*------------*---------*---------------*\n");
     }
 
     public List<Cat> sortByAverageLvl(List<Cat> cats) {
